4f924558b47734e864d9cdb743071608ce5a7729
christopheleduc <christophe.leduc@le-campus-numerique.fr>
2019-04-12 13:33:58 +0200

    Ex0 F0ur V0.1

--- exo-4/script.js
+++ exo-4/script.js
@@ -1,1 +1,18 @@
 console.log("exercice 4");
+
+
+// 1) si l'utilisateur clique sur le bouton 'signup' alors lancer une alerte pour lui dire : "Merci nous vous tiendrons informé des différentes offres";
+
+$( ".navbar-right>li:first-child" ).click(function() {
+    alert( "Merci nous vous tiendrons informé des différentes offres" );
+  });
+
+// 2) récupérer maintenant le contenu de l'input pour adapter votre message : 
+// "Merci adresse email rentrée, nous vous tiendrons informé des différentes offres`;
+
+
+// 3) si l'utilisateur double clique sur l'onglet 'Produits' alors cacher l'onglet 'Produits';
+
+
+// 4) si l'utilisateur clique sur une image alors changer le bouton 'Cart' en haut à droite pour 
+// marquer le fait qu'un produit a été ajouté au panier. S'il clique deux fois on doit avoir le texte Cart(2);
