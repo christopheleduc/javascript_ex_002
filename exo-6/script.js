a3f1dc2fbed466442de3d3c242c97a135db4bd84
christopheleduc <christophe.leduc@le-campus-numerique.fr>
2019-04-16 11:24:47 +0200

    Ex0 Six V0.1

--- exo-6/script.js
+++ exo-6/script.js
@@ -17,29 +17,20 @@ console.log("exercice 6");
 // Affichez ces valeurs dans la console dans un premier temps.
 
 
-$(document).ready(function(){
-    $("#Submit_01" ).click(function(e) {
-    //$('#debug').empty();
-    
-    let user=$('#InputEmail_01').val();
-    var pass=$('#InputPass_01').val();
-
-    if (user.length == 0) {
-        //$('#dvValue').html('Please enter something');
-        alert('Please enter your email adress');
+$(document).ready(function(){ //ecoute
+    $("#Submit_01" ).click(function(e) { //attend clic submit
+    let user=$('#InputEmail_01').val(); // prend valeur input mail
+    var pass=$('#InputPass_01').val(); // prend valeur input password
+    if (user.length > 5 && pass.length > 5) {
+        var strMD5 = $().crypt({
+            method: "sha1",
+            source: pass
+        })
+        console.log("SHA1 string of <b>" + pass + "</b> is <b>" + strMD5 + "</b>");
+        alert( 'User : '+user+' / Hash (SHA1) du mot de pass : '+strMD5);
     }
-    if (pass.length == 0) {
-        //$('#dvValue').html('Please enter something');
-        alert('Please enter something');
-    } else {
-    var strMD5 = $().crypt({
-        method: "sha1",
-        source: pass
+    if (user.length == 0 | pass.length == 0) { // verifie qu'on fait pas n'importe koi
+        alert('Please enter your email adress and a password!'); // on dit: "ça suffit maintenant!!!"
+        }
     });
-    console.log("SHA1 string of <b>" + pass + "</b> is <b>" + strMD5 + "</b>");
-    }
-    //if(auteur != '' && editeur != 'Aucun')
-    console.log( 'User : '+user+' / Password : '+pass);
-    alert( 'User : '+user+' / Password : '+pass+' / SHA1 : '+strMD5);
-      });
 });
